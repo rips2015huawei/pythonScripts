528b11d9a84492e9e98143c33ae2ed0ebe2a027e
Rachael Keller <rkell18@lsu.edu>
2015-06-16 11:55:29 +0800

    Added function to handle different input from 2014 csv files.

--- handleBikeshare.py
+++ handleBikeshare.py
@@ -26,17 +26,26 @@ def formatDuration(str_):
 
     info = str_.split(':')
     noHours = int(info[0])
-    noMin  = int(info[1])
+    noMin   = int(info[1])
     noSec   = int(info[2])
     
     time = float(noHours * 60 + noMin + (noSec * (1/60)))
    
     return time
 
+def changeYearFormat(str_):
+    str_ = str_.replace('-','/')
+    str_ = str_.replace('2014','14')
+    str_ = str_.replace('2015','15')
+    info = str_.split('/');
+    info2 = info[2].split(' ');
+    if ((info[0] == '14') | (info[0] == '15')):
+        str_ = info[1] + '/' + info2[0] +'/'+ info[0] + ' ' + info2[1]
+    return str_
 
 
 # Read in all csv files from current directoryy
-allFiles = glob.glob("SystemData/2012/*.csv");
+allFiles = glob.glob("SystemData/2014/*.csv");
 data = pd.DataFrame()
 combinedData = []
 for file_ in allFiles:
@@ -46,6 +55,8 @@ for file_ in allFiles:
            ,header = 0)
     format_ = '%m/%d/%y %H:%M'
     df[c1] = df[c1].apply(formatDuration)
+    df[c2] = df[c2].apply(changeYearFormat)
+    df[c4] = df[c4].apply(changeYearFormat)
     df[c2] = pd.to_datetime(df[c2], format = format_)
     df[c4] = pd.to_datetime(df[c4], format = format_)
     combinedData.append(df)
@@ -57,15 +68,16 @@ print data.head()
 casuals = pd.DataFrame(data.loc[data[c7] == 'Casual'])
 registered = pd.DataFrame(data.loc[data[c7]=='Registered'])
 
+print'\n~~~~~~2014 data~~~~~~~~~~\n'
+
 print 'Casual Mean, Median Duration:'
 print casuals[c1].mean(), ' , ' ,casuals[c1].median()
-print ' w/ stdev:'
-print casuals[c1].std() 
+print ' w/ stdev and total count'
+print casuals[c1].std() , ' with ', len(casuals[c1]) , ' total people'
 print '\nRegistered Mean, Median Duration:'
 print registered[c1].mean(), ' , ', registered[c1].median()
 print ' w/ stdev:'
-print registered[c1].std()
-
+print registered[c1].std(), ' with ', len(registered[c1]), 'total people'
 
 shortTrips = data.loc[data[c1] < 120]
 print "\n\n\n";
